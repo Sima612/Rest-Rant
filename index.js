12393e6f8cc434d3def7f5df36f739e93bec1dff
Sima612 <sima101531@yahoo.com>
2022-03-23 15:42:53 -0700

    adding home page view

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// DEFINING VIEW ENGINE *JSX*
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 // IMPORTING ROUTER
 app.use('/places', require('./controllers/places'))
 
 // GETTING HOMEPAGE
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('home')
 })
 
 // ERROR ROUTE
