9a1bf55c62c3ee6786b43fffe995862ce3e85252
Sima612 <sima101531@yahoo.com>
2022-03-26 15:27:57 -0700

    added footer and state dropdown

--- views/default.jsx
+++ views/default.jsx
@@ -5,11 +5,31 @@ function Def(html) {
         <html>
             <head>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.11.0/mdb.min.css" rel="stylesheet"/>
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
             <link rel="stylesheet" href="/css/style.css"></link>
             <title>REST-RANT</title>
             </head>
             <body>
-                {html.children}
+                <div className='wrapper'>
+                    {html.children}
+
+                    <div className='push'></div>
+                </div>
+                <footer class="bg-light text-center text-white">
+                    {/*<!-- Grid container -->*/}
+                    <div class="container p-4 pb-0">
+                        {/*<!-- Section: Social media -->*/}
+                        <section class="mb-4">
+                        {/*<!-- Instagram -->*/}
+                        <a class="btn btn-primary btn-floating m-1" href="https://www.myperfectresume.com/me/b554-220122160957" role="button"><i class="fab fa-file"></i></a>
+                        {/*<!-- Linkedin -->*/}
+                        <a class="btn btn-primary btn-floating m-1" href="https://www.linkedin.com/in/rachasima-tran-073808168/" role="button" ><i class="fab fa-linkedin-in"></i></a>
+                        {/*<!-- Github -->*/}
+                        <a class="btn btn-primary btn-floating m-1" href="https://github.com/Sima612" role="button"><i class="fab fa-github"></i></a>
+                        </section>
+                    </div>
+                </footer>
             </body>
         </html>
     )
