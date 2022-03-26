bc5ff693fff8fb72ac5959d2d5266c6bf2f0302d
Sima612 <sima101531@yahoo.com>
2022-03-26 13:47:15 -0700

    can add new places

--- controllers/places.js
+++ controllers/places.js
@@ -1,30 +1,24 @@
 // ROUTER
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 // ROUTE FOR NEW PLACES
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
+// POST /places ROUTE
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
+    if(!req.body.pic) {req.body.pic = 'https://www.graduatehotels.com/berkeley/wp-content/uploads/sites/7/2019/11/9-Graduate_Hotel_256_1600x1200-1330x880-c-default.jpg'}
+    if(!req.body.city) {req.body.city = 'https://thumbs.dreamstime.com/b/random-san-francisco-city-street-downtown-81047097.jpg'}
+    if(!req.body.state) {req.body.state = 'USA'}
+
+    places.push(req.body)
+    res.redirect('/places') // REDIRECTING TO ANOTHER ROUTE
 })
 
+// INDEX ROUTE
 router.get('/', (req,res) => {
-    let places = [{
-        state: 'MA',
-        city: 'Worcester',
-        name: 'Taste-Tea Rolls',
-        cuisines: 'Desserts, Ice-creams',
-        pic: '/images/taste-tea.jpeg'
-    }, {
-        state: 'CA',
-        city: 'Long Beach',
-        name: 'Monorom Cambodian Restaurant',
-        cuisines: 'Cambodian Food',
-        pic: '/images/cambodian.jpeg'
-    }]
     res.render('places/index', {places})
 })
 
---
+++ models/places.js
@@ -0,0 +1,15 @@
+// FILE TO HOLD OUR PLACES ARRAY
+
+module.exports = [{
+    state: 'MA',
+    city: 'Worcester',
+    name: 'Taste-Tea Rolls',
+    cuisines: 'Desserts, Ice-creams',
+    pic: '/images/taste-tea.jpeg'
+}, {
+    state: 'CA',
+    city: 'Long Beach',
+    name: 'Monorom Cambodian Restaurant',
+    cuisines: 'Cambodian Food',
+    pic: '/images/cambodian.jpeg'
+}]
