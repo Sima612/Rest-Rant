99adca8bcffc169849a31abf9f87b023a9b26c4d
Sima612 <sima101531@yahoo.com>
2022-03-28 09:47:55 -0700

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -22,5 +22,14 @@ router.get('/', (req,res) => {
     res.render('places/index', {places})
 })
 
+// SHOW ROUTE
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {res.render('error404')}
+    else if(!places[id]) {res.render('error404')}
+    else {res.render('places/show')}
+
+    res.render('places/show', {places: places[id]})
+})
 
 module.exports = router
