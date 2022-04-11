cbe865cf0c3c7f1a5a5ae7003455e7848ff49d94
Sima612 <sima101531@yahoo.com>
2022-04-11 09:09:42 -0700

    added sum and average rating

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,26 @@ function show(data) {
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className='inactive'>
+            Not yet rated
+        </h3>
+    )
     //Adding logic to comments
+
+    if(data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+
+        let averageRating = sumRatings / data.place.comments.length
+        rating = (
+            <h3>
+                {Math.round(averageRating)} stars
+            </h3>
+        )
+    }
+
     if(data.place.comments.length) {
         comments = data.place.comments.map(c => {
             return (
@@ -40,7 +59,7 @@ function show(data) {
                         </h2>
                         
                         <h2>Rating</h2>
-                        <p>Not Rated</p>
+                        <p>{rating}</p>
                         
                         <h2>Description</h2>
                         <h3>
